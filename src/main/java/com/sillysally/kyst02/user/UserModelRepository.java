1335955534288470a4f37b910b72723855f2bd95
Sally Resch <sallyresch27@gmail.com>
2023-01-30 11:04:43 +0100

    Made UserModelRepository.java

---
+++ src/main/java/com/sillysally/kyst02/user/UserModelRepository.java
@@ -0,0 +1,9 @@
+package com.sillysally.kyst02.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserModelRepository extends JpaRepository<UserModel, Long> {
+    UserModel findByUsername(String username);
+}
