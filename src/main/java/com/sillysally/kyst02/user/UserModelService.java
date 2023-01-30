54d354043db208e4abeddf370eda97510b6a82ce
Sally Resch <sallyresch27@gmail.com>
2023-01-30 11:21:17 +0100

    Reimplemented dependensiec for security and made UserModelService.java

---
+++ src/main/java/com/sillysally/kyst02/user/UserModelService.java
@@ -0,0 +1,22 @@
+package com.sillysally.kyst02.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserModelService implements UserDetailsService {
+
+    private final UserModelRepository userModelRepository;
+
+
+    public UserModelService(UserModelRepository userModelRepository) {
+        this.userModelRepository = userModelRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userModelRepository.findByUsername(username);
+    }
+}
