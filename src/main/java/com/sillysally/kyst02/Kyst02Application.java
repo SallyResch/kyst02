84f4d8df99c5a1ee994f1cdd91ad904f31196072
Sally Resch <sallyresch27@gmail.com>
2023-01-30 10:36:16 +0100

    Initial commit

---
+++ src/main/java/com/sillysally/kyst02/Kyst02Application.java
@@ -0,0 +1,13 @@
+package com.sillysally.kyst02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kyst02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kyst02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sillysally/kyst02/Kyst02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sillysally.kyst02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kyst02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
